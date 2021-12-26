49baf1232fa43e3d3b8837ab3ddfaef05ac947f5
Yurino Oga <yurino.o@latona.io>
2021-12-26 20:45:51 +0900

    release

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	sap_api_caller "sap-api-integrations-maintenance-order-confirmation-reads/SAP_API_Caller"
+	"sap-api-integrations-maintenance-order-confirmation-reads/sap_api_input_reader"
+
+	"github.com/latonaio/golang-logging-library/logger"
+)
+
+func main() {
+	l := logger.NewLogger()
+	fr := sap_api_input_reader.NewFileReader()
+	inoutSDC := fr.ReadSDC("./Inputs/SDC_Maintenance_Order_Confirmation_sample.json")
+	caller := sap_api_caller.NewSAPAPICaller(
+		"https://sandbox.api.sap.com/s4hanacloud/sap/opu/odata/sap/", l,
+	)
+	
+    accepter := inoutSDC.Accepter
+	if len(accepter) == 0 || accepter[0] == "All" {
+		accepter = []string{
+			"Header",
+		}
+	}
+	
+	caller.AsyncGetMaintenanceOrderConfirmation(
+		inoutSDC.MaintenanceOrderConfirmation.MaintOrderConf,
+		accepter,
+	)
+}
